0d43c1598465de8b94f7b4352016676a6229a95e
Jue.Lei <zhuzhilei30@163.com>
2017-07-12 13:45:55 +0800

    v1.0.3-2017.7.12-添加了光流算法的代码

---
+++ include/ASiftDetector.h
@@ -0,0 +1,40 @@
+//
+//  ASiftDetector.h
+//  sift_asift_match
+//
+//  Created by willard on 8/18/15.
+//  Copyright (c) 2015 wilard. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/features2d/features2d.hpp>
+
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+
+using namespace cv;
+
+#ifndef M_PI
+#define M_PI 3.1415926
+
+#endif
+
+class ASiftDetector
+{
+public:
+    ASiftDetector();
+
+    void detectAndCompute(const Mat& img, std::vector< KeyPoint >& keypoints, Mat& descriptors);
+
+private:
+    void affineSkew(double tilt, double phi, Mat& img, Mat& mask, Mat& Ai);
+};
